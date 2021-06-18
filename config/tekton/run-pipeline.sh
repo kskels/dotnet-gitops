43c5070ba60444c3c2285859f4164e3aa202707a
Kaspars Skels <kaspars.skels@gmail.com>
2021-06-18 12:14:02 -0500

    Update structure of config files
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- config/tekton/run-pipeline.sh
+++ config/tekton/run-pipeline.sh
@@ -1,5 +1,5 @@
 tkn pipeline start dotnet-build-and-deploy \
     -w name=shared-workspace,volumeClaimTemplateFile=pvc-source.yaml \
     -p deployment-name=aspnet-core-app \
-    -p git-url=https://gitlab.kskels.com/demos/azure-cache-redis-samples.git \
-    -p IMAGE=image-registry.openshift-image-registry.svc:5000/dotnet-apps-dev/aspnet-core-app
+    -p git-url=https://github.com/kskels/azure-cache-redis-samples.git \
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/dotnet-cicd/aspnet-core-app
